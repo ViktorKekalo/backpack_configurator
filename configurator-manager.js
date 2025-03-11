2e53144c06a17ebfc00b2cd54c72fc338e03f900
ViktorKekalo <111426159+ViktorKekalo@users.noreply.github.com>
2025-03-11 19:43:43 +0200

    Add files via upload

---
+++ configurator-manager.js
@@ -0,0 +1,119 @@
+import * as THREE from "three";
+import { GLTFLoader } from "https://unpkg.com/three@0.138.0/examples/jsm/loaders/GLTFLoader.js";
+
+const BodyColors = {
+   Brown: new THREE.Color(0x8B4512),
+   Black: new THREE.Color(0x1A1A1A),
+   Blue: new THREE.Color(0x104D97)
+};
+
+const MetallColors = {
+   Silver: new THREE.Color(0xFFFFFF),
+   Gold: new THREE.Color(0xFFD500),
+   Black: new THREE.Color(0x1A1A1A),
+}
+
+const BodyMaterials = {
+   Leather: 'leather',
+   Denim: 'denim',
+   Fabric: 'fabric'
+}
+
+const modelPath = './resourses/model/backpack.glb';
+const bodyName = 'Mesh';
+const metallName = 'Mesh_1';
+const texturesPath = './resourses/textures/';
+const bodyBaseTextureName = '_baseColor.jpg';
+const bodyNormalTextureName = '_normal.jpg';
+const bodyocclusionRoughnessMetallicTextureName = '_occlusionRoughnessMetallic.jpg';
+
+class BodyTextures {
+   baseColor;
+   normal;
+   occlusionRoughnessMetallic;
+
+   constructor(baseColor, normal, occlusionRoughnessMetallic) {
+      this.baseColor = baseColor;
+      this.normal = normal;
+      this.occlusionRoughnessMetallic = occlusionRoughnessMetallic;
+   }
+
+   checkIfNeededObject(name) {
+      return (
+         this.baseColor.name === `${name}${bodyBaseTextureName}` &&
+         this.normal.name === `${name}${bodyNormalTextureName}` &&
+         this.occlusionRoughnessMetallic.name === `${name}${bodyocclusionRoughnessMetallicTextureName}`
+      );
+   }
+}
+
+class Backpack {
+
+   #textureLoader = new THREE.TextureLoader();
+   #glbModelLoader = new GLTFLoader();
+   #backpackModel;
+   #backpackBody;
+   #backpackMetall;
+   #activeBodyColor = BodyColors.Brown;
+   #activeMetallColor = MetallColors.Silver;
+   #activeBodyMaterial = BodyMaterials.Leather;
+   #bodyTextures = [];
+
+   async init() {
+      let model = await this.#glbModelLoader.loadAsync(modelPath);
+      this.#backpackModel = model.scene.children[0];
+      this.#backpackBody = this.#backpackModel.getObjectByName(bodyName);
+      this.#backpackBody.material.color = this.#activeBodyColor;
+      this.#backpackMetall = this.#backpackModel.getObjectByName(metallName);
+      this.#backpackMetall.material.color = this.#activeMetallColor;
+      this.#backpackBody.material.map.name = this.#activeBodyMaterial + bodyBaseTextureName;
+      this.#backpackBody.material.normalMap.name = this.#activeBodyMaterial + bodyNormalTextureName;
+      this.#backpackBody.material.roughnessMap.name = this.#activeBodyMaterial + bodyocclusionRoughnessMetallicTextureName;
+      let texturesSet = new BodyTextures(this.#backpackBody.material.map, this.#backpackBody.material.normalMap, this.#backpackBody.material.roughnessMap);
+      this.#bodyTextures.push(texturesSet);
+      console.log(this.#bodyTextures);
+      return this.#backpackModel;
+   }
+
+   changeBodyColor(color) {
+      this.#activeBodyColor = color;
+      this.#backpackBody.material.color = this.#activeBodyColor;
+   }
+
+   changeMetallColor(color) {
+      this.#activeMetallColor = color;
+      this.#backpackMetall.material.color = this.#activeMetallColor;
+   }
+
+   changeTextures(name) {
+      this.#activeBodyMaterial = name;
+      let texturesSet = this.#bodyTextures.find(texture => texture.checkIfNeededObject(this.#activeBodyMaterial));
+
+      if (!texturesSet) {
+         texturesSet = this.#downloadTextures();
+      }
+      this.#backpackBody.material.map = texturesSet.baseColor;
+      this.#backpackBody.material.normalMap = texturesSet.normal;
+      this.#backpackBody.material.roughnessMap = texturesSet.occlusionRoughnessMetallic;
+      this.#backpackBody.material.metalnessMap = texturesSet.occlusionRoughnessMetallic;
+      this.#backpackBody.material.aoMap = texturesSet.occlusionRoughnessMetallic;
+   }
+
+   #downloadTextures() {
+      let base = this.#textureLoader.load(texturesPath + this.#activeBodyMaterial + bodyBaseTextureName);
+      base.name = this.#activeBodyMaterial + bodyBaseTextureName;
+      let normal = this.#textureLoader.load(texturesPath + this.#activeBodyMaterial + bodyNormalTextureName);
+      normal.name = this.#activeBodyMaterial + bodyNormalTextureName;
+      let occlusionRoughnessMetallic = this.#textureLoader.load(texturesPath + this.#activeBodyMaterial + bodyocclusionRoughnessMetallicTextureName);
+      occlusionRoughnessMetallic.name = this.#activeBodyMaterial + bodyocclusionRoughnessMetallicTextureName;
+      let texturesSet = new BodyTextures(base, normal, occlusionRoughnessMetallic);
+      this.#bodyTextures.push(texturesSet);
+      return texturesSet;
+   }
+
+   checkForLoadedTextures() {
+
+   }
+}
+
+export { Backpack, BodyColors, BodyMaterials, MetallColors }
---
+++ configurator-scene.js
@@ -0,0 +1,138 @@
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three@0.138.0/examples/jsm/controls/OrbitControls.js";
+import { Backpack, BodyColors, MetallColors, BodyMaterials } from "./configurator-manager.js";
+
+const sceneWrapper = document.querySelector('.scene-wrapper');
+const brownBodyButton = document.getElementById('body-brown');
+const blackBodyButton = document.getElementById('body-black');
+const blueBodyButton = document.getElementById('body-blue');
+const silverMetallColor = document.getElementById('metall-silver');
+const goldMetallColor = document.getElementById('metall-gold');
+const blackMetallColor = document.getElementById('metall-black');
+const leatherMaterialButton = document.getElementById('material-leather');
+const fabricMaterialButton = document.getElementById('material-fabric');
+const denimMaterialButton = document.getElementById('material-denim');
+const qrPopup = document.querySelector('.qr-popup');
+const closePopupButton = document.querySelector('.close-popup-button');
+const startARButton = document.querySelector('.start-ar');
+const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
+
+let scene, camera, renderer, controls;
+let backpackManager, backpackModel;
+
+blackBodyButton.onclick = () => {
+   let bodyColorButtons = document.querySelectorAll('.body-color-button');
+   bodyColorButtons.forEach(button => button.classList.remove('selected'));
+   blackBodyButton.classList.add('selected');
+   backpackManager.changeBodyColor(BodyColors.Black);
+}
+
+brownBodyButton.onclick = () => {
+   let bodyColorButtons = document.querySelectorAll('.body-color-button');
+   bodyColorButtons.forEach(button => button.classList.remove('selected'));
+   brownBodyButton.classList.add('selected');
+   backpackManager.changeBodyColor(BodyColors.Brown);
+}
+
+blueBodyButton.onclick = () => {
+   let bodyColorButtons = document.querySelectorAll('.body-color-button');
+   bodyColorButtons.forEach(button => button.classList.remove('selected'));
+   blueBodyButton.classList.add('selected');
+   backpackManager.changeBodyColor(BodyColors.Blue);
+}
+
+leatherMaterialButton.onclick = () => {
+   let bodyMatButtons = document.querySelectorAll('.material-button');
+   bodyMatButtons.forEach(button => button.classList.remove('selected'));
+   leatherMaterialButton.classList.add('selected');
+   backpackManager.changeTextures(BodyMaterials.Leather);
+}
+
+fabricMaterialButton.onclick = () => {
+   let bodyMatButtons = document.querySelectorAll('.material-button');
+   bodyMatButtons.forEach(button => button.classList.remove('selected'));
+   fabricMaterialButton.classList.add('selected');
+   backpackManager.changeTextures(BodyMaterials.Fabric);
+}
+
+denimMaterialButton.onclick = () => {
+   let bodyMatButtons = document.querySelectorAll('.material-button');
+   bodyMatButtons.forEach(button => button.classList.remove('selected'));
+   denimMaterialButton.classList.add('selected');
+   backpackManager.changeTextures(BodyMaterials.Denim);
+}
+
+silverMetallColor.onclick = () => {
+   let metallColorButtons = document.querySelectorAll('.metall-color-button');
+   metallColorButtons.forEach(button => button.classList.remove('selected'));
+   silverMetallColor.classList.add('selected');
+   backpackManager.changeMetallColor(MetallColors.Silver);
+}
+
+goldMetallColor.onclick = () => {
+   let metallColorButtons = document.querySelectorAll('.metall-color-button');
+   metallColorButtons.forEach(button => button.classList.remove('selected'));
+   goldMetallColor.classList.add('selected');
+   backpackManager.changeMetallColor(MetallColors.Gold);
+}
+
+blackMetallColor.onclick = () => {
+   let metallColorButtons = document.querySelectorAll('.metall-color-button');
+   metallColorButtons.forEach(button => button.classList.remove('selected'));
+   blackMetallColor.classList.add('selected');
+   backpackManager.changeMetallColor(MetallColors.Black);
+}
+
+startARButton.onclick = () => {
+   if (!isMobileDevice) {
+      qrPopup.classList.add('show');
+   }
+}
+
+closePopupButton.onclick = () => {
+   qrPopup.classList.remove('show');
+}
+
+init().then(animate);
+
+async function init() {
+   scene = new THREE.Scene();
+   const { width, height } = sceneWrapper.getBoundingClientRect();
+   camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10);
+   camera.position.set(0, 1, 0.75);
+   camera.updateProjectionMatrix();
+   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+   renderer.setSize(width, height);
+   sceneWrapper.appendChild(renderer.domElement);
+   setupLights(scene);
+   controls = new OrbitControls(camera, renderer.domElement);
+   controls.enableDamping = true;
+   backpackManager = new Backpack();
+   backpackModel = await backpackManager.init();
+   scene.add(backpackModel);
+}
+
+// Animation loop
+function animate() {
+   requestAnimationFrame(animate);
+   controls.update();
+   renderer.render(scene, camera);
+}
+
+// Handle window resize
+window.addEventListener("resize", () => {
+   const { width, height } = sceneWrapper.getBoundingClientRect();
+   camera.aspect = width / height;
+   camera.updateProjectionMatrix();
+   renderer.setSize(width, height);
+});
+
+function setupLights(scene) {
+   const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+   dirLight.position.set(5, 5, 5);
+   dirLight.castShadow = true;
+   scene.add(dirLight);
+   const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+   scene.add(ambientLight);
+}
+
